99850385adcd051fa94942647e47e112f73c78f9
765199214 <765199214@qq.com>
2020-10-14 12:38:07 +0800

    多个流控配置都持久化至nacos中

--- cloudalibaba-sentinel-nacosCHIJIU-9100/src/main/java/linkpower/controller/OrderController.java
+++ cloudalibaba-sentinel-nacosCHIJIU-9100/src/main/java/linkpower/controller/OrderController.java
@@ -1,5 +1,7 @@
 package linkpower.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.sun.deploy.security.BlockedException;
 import linkpower.service.IOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class OrderController {
@@ -21,4 +24,22 @@ public class OrderController {
         log.info("----getOrder---- id={}",id);
         return oserService.getOrder(id);
     }
+
+    // 测试两个流控配置持久化
+    @SentinelResource(value = "test",blockHandler ="fallbackhandler",fallback = "fallback") //采取sentinelresources测试
+    @RequestMapping("/product/getProduct2/{id}")
+    public String getOrder2(@PathVariable("id") String id){
+        log.info("----getOrder2---- id={}",id);
+        return oserService.getOrder(id);
+    }
+
+    public String fallbackhandler(String id,BlockedException e){
+        log.info("----getOrder2--fallbackhandler");
+        return "SentinelResource test fallbackhandler  请求降级！！";
+    }
+    public String fallback(String id){
+        log.info("----getOrder2--fallback");
+        return "SentinelResource test  fallback  请求降级！！";
+    }
+
 }
